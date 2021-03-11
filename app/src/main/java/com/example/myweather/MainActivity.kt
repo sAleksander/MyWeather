58b9834bd7a37fa668072ea74223b88cdc173a52
Aleksander Szulc <szulcalek@gmail.com>
2021-03-11 18:38:09 +0100

    normal view done

--- app/src/main/java/com/example/myweather/MainActivity.kt
+++ app/src/main/java/com/example/myweather/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.myweather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_main)
     }
 }
