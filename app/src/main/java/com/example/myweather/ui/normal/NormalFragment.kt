540021e257361241679c17212548c92f5496ca29
Aleksander Szulc <szulcalek@gmail.com>
2021-03-14 12:56:44 +0100

    ui done

--- app/src/main/java/com/example/myweather/ui/normal/NormalFragment.kt
+++ app/src/main/java/com/example/myweather/ui/normal/NormalFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.Window
+import androidx.navigation.fragment.findNavController
 import com.example.myweather.R
 
 class NormalFragment : Fragment() {
